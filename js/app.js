51c57ed7ba7d49e81ca2eacdc803e1911afb1e0e
Samuel Rysdyk <samuel.rysdyk@gmail.com>
2016-06-27 22:20:47 -0400

    add basic angular functionality

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var draftApp = angular.module('draftApp', []);
+
+draftApp.controller('PlayerListController', function PlayerListController($scope, $http){
+  $http.get('../lib/players.json')
+    .then(function(data){
+      $scope.players = data.data.players;
+    });
+})
