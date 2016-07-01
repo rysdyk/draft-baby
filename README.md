05d4d217a3236faa260d0b135c4d4ae70e0422a4
Samuel Rysdyk <samuel.rysdyk@gmail.com>
2016-07-01 17:53:10 -0400

    add more gulp dependencies and cleaned up files

---
+++ README.md
@@ -0,0 +1,1 @@
+[Instructions](http://mherman.org/blog/2014/08/14/kickstarting-angular-with-gulp/)
--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,37 @@ draftApp.controller('PlayerListController', function PlayerListController($scope
   $scope.greaterThan = function(prop, val){
     return function(item){
       return item[prop] > val;
-    }
+    };
+  };
+});
+
+function draftPick(t, p){
+  var teams = t;
+  var pick = p;
+  var snakePick = teams * 2 - pick + 1;
+  var pickSpread = teams * 2;
+
+  for(i=pick; i<330; i+= pickSpread ) {
+    console.log(i);
+    $("tr:eq(" + i + ")").addClass("draftpick");
   }
-})
+
+  for(i=snakePick; i<330; i+= pickSpread ) {
+    console.log(i);
+    $("tr:eq(" + i + ")").addClass("draftpick");
+  }
+}
+function draftPosClear(){
+  $("tr").removeClass("draftpick");
+}
+
+$('#draftPosEst').click( function(){
+  draftPosClear();
+  var teams = parseInt($('#draftPosTeams').val());
+  var pick = parseInt($('#draftPos').val());
+  draftPick(teams, pick);
+});
+
+$('#draftPosClear').click( function(){
+  draftPosClear();
+});
