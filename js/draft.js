7077b7b9749eede67f78e23ff8359f346d8668b6
sam rysdyk <samuel.rysdyk@gmail.com>
2017-06-04 20:24:34 -0500

    make draft.js follow object oriented pattern

--- js/draft.js
+++ js/draft.js
@@ -1,64 +1,101 @@
-// draft
-var drafted;
-var nodeRows = document.querySelector('tbody').childNodes;
-var list = document.getElementById('drafted');
+(function(){
+  var draftBaby = {
 
-if (localStorage.drafted) {
-  drafted = JSON.parse(localStorage.drafted)
-} else {
-  drafted = [];
-}
+    init: function(){
+      this.cacheDom();
+      this.setDrafted();
+      this.catchUp();
+      this.addClick();
+      this.setLocal();
+      //this.undoDraft();
+    },
 
-catchUp();
+    cacheDom: function() {
+      this.drafted;
+      this.nodeRows = document.querySelector('tbody').childNodes;
+      this.list = document.getElementById('drafted');
+    },
 
-nodeRows.forEach(function(row){
-  row.addEventListener('click', function(){
-    this.style.display = 'none';
-    drafted.push(this.children[1].innerText);
-    showDrafted(this);
-    setLocal();
-  })
-})
+    setDrafted: function(){
+      if (localStorage.drafted.length > 2) {
+        this.drafted = JSON.parse(localStorage.drafted)
+      } else {
+        this.drafted = [];
+      }
+    },
 
-function showDrafted(player) {
-  var item = document.createElement('li');
-  item.appendChild(document.createTextNode(player.children[1].innerText));
-  list.appendChild(item);
-}
+    catchUp: function() {
+      var drafted = this.drafted;
+      var list = this.list;
+      this.nodeRows.forEach(function(row){
+        var data = row.childNodes[1].innerText;
+        if (drafted.includes(data)) {
+          row.style.display = 'none';
+          var item = document.createElement('li');
+          item.appendChild(document.createTextNode(data));
+          list.appendChild(item);
+        }
+      })
+    },
 
-// undo draft
-function undoDraft(e) {
-  e.preventDefault();
-  var name = drafted.pop();
-  list.removeChild(list.lastChild);
-  nodeRows.forEach(function(row){
-    var data = row.childNodes[1].innerText;
-    if (name == data) {
-      row.style.display = '';
-    }
-  })
+    addClick: function() {
+      var drafted = this.drafted;
+      var showDrafted = this.showDrafted;
+      var setLocal = this.setLocal;
+      var undo = document.getElementById('undo-draft');
+      var undoDraft = this.undoDraft;
 
-  // remove from local storage
-  setLocal();
-}
+      this.nodeRows.forEach(function(row){
+        row.addEventListener('click', function(){
+          this.style.display = 'none';
+          drafted.push(this.children[1].innerText);
+          showDrafted(this);
+          setLocal();
+        })
+      });
 
+      undo.addEventListener('click', function(e){
+        undoDraft(e);
+      })
+    },
 
-function setLocal() {
-  if (typeof(Storage) !== "undefined") {
-    localStorage.setItem('drafted', JSON.stringify(drafted) )
-  } else {
-    console.log("Sorry, no local storage")
-  }
-}
-
-function catchUp() {
-  nodeRows.forEach(function(row){
-    var data = row.childNodes[1].innerText;
-    if (drafted.includes(data)) {
-      row.style.display = 'none';
+    showDrafted: function(player) {
+      var list = document.getElementById('drafted');
       var item = document.createElement('li');
-      item.appendChild(document.createTextNode(data));
+      item.appendChild(document.createTextNode(player.children[1].innerText));
       list.appendChild(item);
+    },
+
+    setLocal: function() {
+      var drafted = this.drafted;
+      if (typeof(Storage) !== "undefined") {
+        localStorage.setItem('drafted', JSON.stringify(drafted) )
+      } else {
+        console.log("Sorry, no local storage")
+      }
+    },
+
+    undoDraft: function(e) {
+      e.preventDefault();
+      var drafted = draftBaby.drafted;
+      var list = draftBaby.list;
+      var nodeRows = draftBaby.nodeRows;
+      var setLocal = draftBaby.setLocal;
+      var name = drafted.pop();
+      list.removeChild(list.lastChild);
+      nodeRows.forEach(function(row){
+        var data = row.childNodes[1].innerText;
+        if (name == data) {
+          row.style.display = '';
+        }
+      })
+      // remove from local storage
+      setLocal();
     }
-  })
-}
+
+
+  }
+
+  draftBaby.init();
+
+})();
